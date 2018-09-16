41596620268c38f4e09ab63f37fab3b1fbdcfe9e
Steffany Brown <steffannbrown@gmail.com>
2018-09-16 12:44:36 -0700

    initial commit with basic structure of retrieveIssues command

--- app.js
+++ app.js
@@ -1,170 +1,96 @@
-// Copyright 2015, Google, Inc. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#!/usr/bin/env node
+// node retrieveIssues.js -i testIssues.json -s testCSV.csv
+
 
 'use strict';
 
-// Activate Google Cloud Trace API
-if (process.env.NODE_ENV === 'production') {
-  require('@google/cloud-trace').start();
+const axios = require('axios');
+const async = require("async");
+const fs = require('fs');
+// const argv = require('yargs').argv;
+const settings = require('./settings.json')
+const Json2csvParser = require('json2csv').Parser;
+const yargs = require('yargs')
+
+yargs
+  .commandDir(join(__dirname, 'lib', 'commands'))
+  .demand(1)
+  .help()
+
+
+async function retrieveIssues(data, file) {
+  // get file from -i flag argument
+  let rawdata = data;
+  let issues = JSON.parse(rawdata);
+
+  const issuesArray = issues.Issues;
+  const promises = issuesArray.map(issue => getIssueInfo(issue));
+
+  // results is now array of current issue information
+  const results = await Promise.all(promises);
+
+  //save results to csv file
+  makeCSV(results, file)
 }
 
-// Express and http server
-let express = require('express');
-let http = require('http');
 
-// Middleware
-let bodyParser = require('body-parser');
-let session = require('cookie-session');
-let csurf = require('csurf');
+/**
+ * takes an issue and returns the curreny title, body, & labels
+ *
+ * @param {object} issue
+ */
+async function getIssueInfo(issue) {
+  let repoName = issue.Repo;
+  let number = issue.IssueID;
+  let inssueInfo = {}
 
-// Initialize IOC container
-let container = require('./container');
+  const githubClientID = settings.githubClientID;
+  const githubClientSecret = settings.githubClientSecret;
 
-// By exporting our Express app creation functionality we can more easily write tests.
-exports.createServer = function () {
+  let url = `https://api.github.com/repos/${repoName}/issues/${number}?client_id=${githubClientID}&client_secret=${githubClientSecret}`;
 
-  // Pull an initial set of dependencies from our IOC container
-  return container.resolve(function (config, utils, auth, logger, jsonpSecurity, ensureAuthenticated, errorHandler, user, repos, models) {
-    let app = express();
+  let response = await axios.get(url);
+  // should this have different response to error?
+  if (response.err) { console.log('error'); }
+  else {
+     let issueInfo = {}
 
-    app.disable('etag');
-    app.use(jsonpSecurity); // Fix res.json to safely handle JSON vulnerability
-    app.use(bodyParser.json()); // parse application/json
+    issueInfo.repoName = repoName;
+    issueInfo.issueNumber = number;
+    issueInfo.title = response.data.title;
+    issueInfo.body = response.data.body;
+    let labelNames = separateLabels(response.data.labels);
+    issueInfo.labels = labelNames;
 
-    // Setup static file folder
-    app.use(express.static(`${__dirname}/public`));
-
-    // Configure the session and session storage.
-    // MemoryStore isn't viable in a multi-server configuration, so we
-    // use encrypted cookies. Redis or Memcache is a great option for
-    // more secure sessions, if desired.
-    app.use(session({
-      secret: config.secret,
-      signed: true
-    }));
-
-    // Receiving a GitHub webhook on issue creation
-    // We mount this route here because the csurf middleware would prevent it
-    // from working otherwise.
-    app.post('/api/repos/:key/hook', utils.makeSafe(repos.hook));
-
-    // Cross-site Request Forgery protection.
-    // A csrf token will automatically be stored in the user's cookie session.
-    app.use(csurf());
-
-    // Set the CSRF cookie
-    app.use(function (req, res, next) {
-      res.cookie('XSRF-TOKEN', req.csrfToken());
-      return next();
-    });
-
-    // Activate our GitHub authentication middleware
-    app.use(auth.initialize());
-    app.use(auth.session());
-
-    // Log every request
-    app.use(logger.requestLogger);
-
-    // GitHub authentication endpoints
-    app.get('/auth/github', auth.authenticate('github', {
-      scope: ['read:org', config.github.accessLevel === 'private' ? 'repo' : 'public_repo']
-    }, function (err, req, res, next) {
-      console.log(err);
-      next(err);
-    }));
-    app.get('/auth/github/callback', auth.authenticate('github', {
-      failureRedirect: '/401'
-    }), function (req, res) {
-      res.redirect('/');
-    });
-
-    app.get('/api/logout', function (req, res) {
-      req.logout();
-      res.redirect('/');
-    });
-
-    // Return the currently authenticated user, if any
-    app.get('/api/user', utils.makeSafe(user.user));
-    // Return the repos accessible by the currently authenticated user, if any
-    app.get('/api/user/repos', ensureAuthenticated, utils.makeSafe(user.repos));
-
-    // Trigger training for the specified model
-    app.post('/api/models/:key/train', ensureAuthenticated, utils.makeSafe(models.trainOne));
-
-    app.route('/api/models')
-      // Return a collection of models
-      .get(ensureAuthenticated, utils.makeSafe(models.findAll))
-      // Create a new model
-      .post(ensureAuthenticated, utils.makeSafe(models.createOne));
-
-    app.route('/api/models/:key')
-      // Return a model
-      .get(ensureAuthenticated, utils.makeSafe(models.findOne))
-      // Update a model
-      .put(ensureAuthenticated, utils.makeSafe(models.updateOne))
-      // Delete a model
-      .delete(ensureAuthenticated, utils.makeSafe(models.destroyOne));
-
-    // [START repo_endpoints]
-    // Search GitHub for a repo with the specified owner and name    
-    app.get('/api/repos/search/:owner/:repo', ensureAuthenticated, utils.makeSafe(repos.search));
-
-    app.route('/api/repos/:key')
-      // Return a repo
-      .get(ensureAuthenticated, utils.makeSafe(repos.findOne))
-      // Update a repo
-      .put(ensureAuthenticated, utils.makeSafe(repos.updateOne))
-      // Delete a repo
-      .delete(ensureAuthenticated, utils.makeSafe(repos.destroyOne));
-
-    app.route('/api/repos')
-      // Return a repo
-      .get(ensureAuthenticated, utils.makeSafe(repos.findAll));
-    // [END repo_endpoints]
-
-    // [START middleware]
-    // Catch any other unknown requests by rendering the home page, where
-    // Angular will take care of the rest.
-    app.get('*', function (req, res, next) {
-      return res.sendFile('index.html', {
-        root: './public/'
-      });
-    });
-
-    // Add the error logger after all middleware and routes so that
-    // it can log errors from the whole application. Any custom error
-    // handlers should go after this.
-    app.use(logger.errorLogger);
-
-    // Catch all handler, assumes error
-    app.use(errorHandler);
-    // [END middleware]
-
-    return app;
-  });
-};
-
-// Only initialize http server if this file is actually being executed as the "main"
-// entry point of the program. The other case is that this file is being pulled into
-// one of our tests.
-if (module === require.main) {
-  var config = container.get('config');
-  var app = exports.createServer();
-  // [START server]
-  var server = http.createServer(app).listen(config.port, config.host, function () {
-    console.log(`App listening at http://${server.address().address}:${server.address().port}`);
-    console.log('Press Ctrl+C to quit.');
-  });
-  // [END server]
+    return issueInfo;
+  }
 }
+
+
+function separateLabels(labelsArray){
+  let newArray = []
+  labelsArray.forEach(function (labelObject){
+    newArray.push(labelObject.name);
+  });
+
+  return newArray;
+}
+
+
+/**
+ * Creates a csv file of issue data
+ * Writes to cli argument provided by -s flag
+ *
+ * @param {array} issues - The issues to group.
+ */
+function makeCSV(issues, file) {
+  const fields = ['repoName', 'issueNumber', 'title', 'body', 'labels'];
+
+  const json2csvParser = new Json2csvParser({ fields, unwind: 'labels' });
+  const csv = json2csvParser.parse(issues);
+
+  fs.writeFileSync(file, csv);
+}
+
+
+// retrieveIssues()
---
+++ node_modules/json2csv/lib/JSON2CSVBase.js
@@ -0,0 +1,283 @@
+'use strict';
+
+const os = require('os');
+const lodashGet = require('lodash.get');
+const lodashSet = require('lodash.set');
+const lodashCloneDeep = require('lodash.clonedeep');
+
+class JSON2CSVBase {
+  constructor(opts) {
+    this.opts = this.preprocessOpts(opts);
+  }
+
+  /**
+   * Check passing opts and set defaults.
+   *
+   * @param {Json2CsvOptions} opts Options object containing fields,
+   * delimiter, default value, quote mark, header, etc.
+   */
+  preprocessOpts(opts) {
+    const processedOpts = Object.assign({}, opts);
+    processedOpts.unwind = !Array.isArray(processedOpts.unwind)
+      ? (processedOpts.unwind ? [processedOpts.unwind] : [])
+      : processedOpts.unwind
+    processedOpts.delimiter = processedOpts.delimiter || ',';
+    processedOpts.flattenSeparator = processedOpts.flattenSeparator || '.';
+    processedOpts.eol = processedOpts.eol || os.EOL;
+    processedOpts.quote = typeof processedOpts.quote === 'string'
+      ? opts.quote
+      : '"';
+    processedOpts.doubleQuote = typeof processedOpts.doubleQuote === 'string'
+      ? processedOpts.doubleQuote
+      : Array(3).join(processedOpts.quote);
+    processedOpts.header = processedOpts.header !== false;
+    processedOpts.includeEmptyRows = processedOpts.includeEmptyRows || false;
+    processedOpts.withBOM = processedOpts.withBOM || false;
+
+    return processedOpts;
+  }
+
+  /**
+   * Create the title row with all the provided fields as column headings
+   *
+   * @returns {String} titles as a string
+   */
+  getHeader() {
+    return this.opts.fields
+      .map(field =>
+        (typeof field === 'string')
+          ? field
+          : (field.label || field.value)
+      )
+      .map(header => this.processValue(header, true))
+      .join(this.opts.delimiter);
+  }
+
+  /**
+   * Preprocess each object according to the give opts (unwind, flatten, etc.).
+   *
+   * @param {Object} row JSON object to be converted in a CSV row
+   */
+  preprocessRow(row) {
+    const processedRow = (this.opts.unwind && this.opts.unwind.length)
+      ? this.unwindData(row, this.opts.unwind)
+      : [row];
+
+    if (this.opts.flatten) {
+      return processedRow.map(row => this.flatten(row, this.opts.flattenSeparator));
+    }
+
+    return processedRow;
+  }
+
+  /**
+   * Create the content of a specific CSV row
+   *
+   * @param {Object} row JSON object to be converted in a CSV row
+   * @returns {String} CSV string (row)
+   */
+  processRow(row) {
+    if (!row
+        || (Object.getOwnPropertyNames(row).length === 0
+          && !this.opts.includeEmptyRows)) {
+      return undefined;
+    }
+
+    return this.opts.fields
+      .map(fieldInfo => this.processCell(row, fieldInfo))
+      .join(this.opts.delimiter);
+  }
+
+  /**
+   * Create the content of a specfic CSV row cell
+   *
+   * @param {Object} row JSON object representing the  CSV row that the cell belongs to
+   * @param {Object} fieldInfo Details of the field to process to be a CSV cell
+   * @returns {String} CSV string (cell)
+   */
+  processCell(row, fieldInfo) {
+    const stringify = typeof fieldInfo === 'object' && fieldInfo.stringify !== undefined
+      ? fieldInfo.stringify
+      : true;
+
+    return this.processValue(this.getValue(row, fieldInfo), stringify);
+  }
+
+  /**
+   * Create the content of a specfic CSV row cell
+   *
+   * @param {Object} row JSON object representing the  CSV row that the cell belongs to
+   * @param {Object} fieldInfo Details of the field to process to be a CSV cell
+   * @returns {Any} Field value
+   */
+  getValue(row, fieldInfo) {
+    const defaultValue = typeof fieldInfo === 'object' && 'default' in fieldInfo
+      ? fieldInfo.default
+      : this.opts.defaultValue;
+
+    let value;
+    if (fieldInfo) {
+      if (typeof fieldInfo === 'string') {
+        value = lodashGet(row, fieldInfo, defaultValue);
+      } else if (typeof fieldInfo === 'object') {
+        if (typeof fieldInfo.value === 'string') {
+          value = lodashGet(row, fieldInfo.value, defaultValue);
+        } else if (typeof fieldInfo.value === 'function') {
+          const field = {
+            label: fieldInfo.label,
+            default: fieldInfo.default
+          };
+          value = fieldInfo.value(row, field);
+        }
+      }
+    }
+
+    return (value === null || value === undefined)
+      ? defaultValue
+      : value;
+  }
+
+  /**
+   * Create the content of a specfic CSV row cell
+   *
+   * @param {Any} value Value to be included in a CSV cell
+   * @param {Boolean} stringify Details of the field to process to be a CSV cell
+   * @returns {String} Value stringified and processed
+   */
+  processValue(value, stringify) {
+    if (value === null || value === undefined) {
+      return undefined;
+    }
+
+    const isValueString = typeof value === 'string';
+    if (isValueString) {
+      value = value
+        .replace(/\n/g, '\u2028')
+        .replace(/\r/g, '\u2029')
+        .replace(/\t/g, '\u21E5');
+    }
+
+    //JSON.stringify('\\') results in a string with two backslash
+    //characters in it. I.e. '\\\\'.
+    let stringifiedValue = (stringify
+      ? JSON.stringify(value)
+      : value);
+
+    if (typeof value === 'object' && !/^"(.*)"$/.test(stringifiedValue)) {
+      // Stringify object that are not stringified to a
+      // JSON string (like Date) to escape commas, quotes, etc.
+      stringifiedValue = JSON.stringify(stringifiedValue);
+    }
+
+    if (stringifiedValue === undefined) {
+      return undefined;
+    }
+
+    if (isValueString) {
+      stringifiedValue = stringifiedValue
+        .replace(/\u2028/g, '\n')
+        .replace(/\u2029/g, '\r')
+        .replace(/\u21E5/g, '\t');
+    }
+
+
+    if (this.opts.quote === '"') {
+      // Replace automatically scaped single quotes by doubleQuotes
+      stringifiedValue = stringifiedValue
+        .replace(/(\\")(?!$)/g, this.opts.doubleQuote);
+    } else {
+      // Unescape automatically escaped double quote symbol
+      // Replace single quote with double quote
+      // Replace wrapping quotes
+      stringifiedValue = stringifiedValue
+        .replace(/(\\")(?!$)/g, '"')
+        .replace(new RegExp(this.opts.quote, 'g'), this.opts.doubleQuote)
+        .replace(/^"(.*)"$/, this.opts.quote + '$1' + this.opts.quote);
+    }
+
+      // Remove double backslashes
+      stringifiedValue = stringifiedValue
+        .replace(/\\\\/g, '\\');
+
+    if (this.opts.excelStrings && typeof value === 'string') {
+      stringifiedValue = '"="' + stringifiedValue + '""';
+    }
+
+    return stringifiedValue;
+  }
+
+  /**
+   * Performs the flattening of a data row recursively
+   *
+   * @param {Object} dataRow Original JSON object
+   * @param {String} separator Separator to be used as the flattened field name
+   * @returns {Object} Flattened object
+   */
+  flatten(dataRow, separator) {
+    function step (obj, flatDataRow, currentPath) {
+      Object.keys(obj).forEach((key) => {
+        const value = obj[key];
+
+        const newPath = currentPath
+          ? `${currentPath}${separator}${key}`
+          : key;
+
+        if (typeof value !== 'object'
+          || value === null
+          || Array.isArray(value)
+          || Object.prototype.toString.call(value.toJSON) === '[object Function]'
+          || !Object.keys(value).length) {
+          flatDataRow[newPath] = value;
+          return;
+        }
+
+        step(value, flatDataRow, newPath);
+      });
+
+      return flatDataRow;
+    }
+
+    return step(dataRow, {});
+  }
+
+  /**
+   * Performs the unwind recursively in specified sequence
+   *
+   * @param {Object} dataRow Original JSON object
+   * @param {String[]} unwindPaths The paths as strings to be used to deconstruct the array
+   * @returns {Array} Array of objects containing all rows after unwind of chosen paths
+  */
+  unwindData(dataRow, unwindPaths) {
+    const unwind = (rows, unwindPath) => {
+      const clone = unwindPath.indexOf('.') !== -1
+        ? o => lodashCloneDeep(o)
+        : o => Object.assign({}, o);
+
+      return rows
+        .map(row => {
+          const unwindArray = lodashGet(row, unwindPath);
+
+          if (!Array.isArray(unwindArray)) {
+            return row;
+          }
+
+          if (!unwindArray.length) {
+            return lodashSet(clone(row), unwindPath, undefined);
+          }
+
+          return unwindArray.map((unwindRow, index) => {
+            const clonedRow = (this.opts.unwindBlank && index > 0)
+              ? {}
+              : clone(row);
+
+            return lodashSet(clonedRow, unwindPath, unwindRow);
+          });
+        })
+        .reduce((a, e) => a.concat(e), []);
+    };
+
+    return unwindPaths.reduce(unwind, [dataRow]);
+  }
+}
+
+module.exports = JSON2CSVBase;
